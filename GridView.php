7625a0bfcfc156883fc8eb8c4057f8e642c20b82
sanchezzzhak <sanchezzzhak@ya.ru>
2015-07-30 10:30:03 +0100

    no message

---
+++ GridView.php
@@ -0,0 +1,19 @@
+<?php
+namespace kak\widgets\grid;
+
+/**
+ * Class GridView
+ * @package kak\widgets\grid
+ */
+class GridView extends \yii\grid\GridView
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        parent::run();
+    }
+} 
