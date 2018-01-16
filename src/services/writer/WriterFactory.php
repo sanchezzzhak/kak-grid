89674d40752d5af53320a3bdce3186cf99ffe545
sanchezzzhak <sanchezzzhak@ya.ru>
2018-01-16 15:50:46 +0300

    fix save txt

--- src/services/writer/WriterFactory.php
+++ src/services/writer/WriterFactory.php
@@ -2,6 +2,7 @@
 
 namespace kak\widgets\grid\services\writer;
 use Box\Spout\Common\Helper\GlobalFunctionsHelper;
+use kak\widgets\grid\interfaces\ExportType;
 
 class WriterFactory extends \Box\Spout\Writer\WriterFactory
 {
@@ -9,13 +10,20 @@ class WriterFactory extends \Box\Spout\Writer\WriterFactory
     public static function create($writerType)
     {
         $writer = null;
-        switch($writerType){
-            case 'json': $writer = new WriterJson; break;
-            case 'text': $writer = new WriterText; break;
-            case 'xml': $writer = new WriterXml; break;
+        switch ($writerType) {
+            case ExportType::JSON:
+                $writer = new WriterJson;
+                break;
+            case ExportType::TXT:
+                $writer = new WriterText;
+                break;
+            case ExportType::XML:
+                $writer = new WriterXml;
+                break;
 //            case 'pdf': break;
 //            case 'txt': break;
-            default: return parent::create($writerType);
+            default:
+                return parent::create($writerType);
         }
         $writer->setGlobalFunctionsHelper(new GlobalFunctionsHelper());
         return $writer;
