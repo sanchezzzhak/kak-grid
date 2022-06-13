8f38d883352537c9cb24cb004a382d95c87824af
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-06-13 22:34:52 +0300

    chore: set php version

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -6,21 +6,20 @@ use Symfony\Component\String\UnicodeString;
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+
+/**
+ * @param string|null $string
+ * @return UnicodeString|ByteString
+ */
+function s(?string $string = '')
+{
+    $string = (string)$string;
+    return preg_match('//u', $string) ? new UnicodeString($string) : new ByteString($string);
+}
+
 $autoloadGitHub = dirname(__DIR__, 1) . '/vendor/autoload.php';
 $yiiGitHub = dirname(__DIR__, 1) . '/vendor/yiisoft/yii2/Yii.php';
 
-
-if (!\function_exists('s')) {
-    /**
-     * @param string|null $string
-     * @return UnicodeString|ByteString
-     */
-    function s(?string $string = '')
-    {
-        $string = (string)$string;
-        return preg_match('//u', $string) ? new UnicodeString($string) : new ByteString($string);
-    }
-}
 
 require_once $yiiGitHub;
 require_once $autoloadGitHub;
