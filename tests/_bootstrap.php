c1171de139485a9ebf43bd742ece74aaabb9f7ae
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-06-13 22:47:04 +0300

    chore: set php version

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -6,7 +6,6 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require_once dirname(__DIR__, 1) . '/vendor/autoload.php';
 require_once dirname(__DIR__, 1) . '/vendor/yiisoft/yii2/Yii.php';
-require_once __DIR__ . '/hack_symphony.php';
 
 Yii::setAlias('@tests', __DIR__);
 Yii::setAlias('@data', __DIR__ . DIRECTORY_SEPARATOR . '_data');
