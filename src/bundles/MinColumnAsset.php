7c3e2c18dfea2d347ac63395a168e306a5a37338
Tutik Alexsandr <sanchezzzhak@ya.ru>
2023-08-09 19:08:21 +0300

    Update MinColumnAsset.php

--- src/bundles/MinColumnAsset.php
+++ src/bundles/MinColumnAsset.php
@@ -1,1 +1,22 @@
 
+<?php
+namespace kak\widgets\grid\bundles;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class GridViewAsset
+ * @package kak\widgets\grid
+ */
+class MinColumnAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/kak/grid/assets';
+
+    public $js = [
+        'min-column.js'
+    ];
+
+    public $css = [
+        'min-column.css'
+    ];
+} 
