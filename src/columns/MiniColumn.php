b6ca055e6af1dcfb60d17b4a5888797f57784e5d
Tutik Alexsandr <sanchezzzhak@ya.ru>
2023-08-09 19:05:59 +0300

    Create MiniColumn.php

---
+++ src/columns/MiniColumn.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace kak\widgets\grid\columns;
+
+use kak\widgets\grid\bundles\MinColumnAsset;
+use kak\widgets\grid\columns\DataColumn;
+use yii\helpers\Html;
+
+/**
+ * Class MiniColumn
+ * @package app\components\columns
+ */
+class MiniColumn extends DataColumn
+{
+    public int $minWidth = 80;
+    public int $maxWidth = 280;
+    public bool $defaultHide = true;
+
+    public function init()
+    {
+        parent::init();
+        MinColumnAsset::register($this->grid->view);
+    }
+
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        $content = parent::renderDataCellContent($model, $key, $index);
+        return sprintf('<p class="grid-overtext-container">%s</p>', $content);
+    }
+
+    protected function renderHeaderCellContent()
+    {
+        $action = Html::button('<i class="fas fa-arrows-alt-h"></i>', [
+            'class' => 'btn btn-xs btn-dark grid-btn-min-column',
+            'data-min-width' => $this->minWidth,
+            'data-max-width' => $this->maxWidth,
+            'data-hide' => (int)$this->defaultHide,
+        ]);
+        $header = parent::renderHeaderCellContent();
+        return sprintf('%s <span class="pull-right">%s</span>', $header, $action);
+    }
+
+}
